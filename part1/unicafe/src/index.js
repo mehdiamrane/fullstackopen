e958ef55929b67094e862bd066d9d6caccb0b6c9
Mehdi Amrane <me.amrane@gmail.com>
2020-04-20 18:26:59 +0200

    Part1/unicafe: 1.8 solution added (#10)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = (props) => (
+  <>
+    <h1>Stats</h1>
+
+    <p>Good: {props.good}</p>
+    <p>Neutral: {props.neutral}</p>
+    <p>Bad: {props.bad}</p>
+
+    <p>Total: {props.total}</p>
+    <p>Average: {props.average}</p>
+    <p>Positive: {props.positivePercentage}%</p>
+  </>
+);
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
@@ -28,6 +42,15 @@ const App = () => {
   let average = allRatings.reduce(reducer, 0) / total;
   let positivePercentage = (good / total) * 100;
 
+  const statisticsProps = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+    average: average,
+    positivePercentage: positivePercentage,
+  };
+
   return (
     <div>
       <h1>Give your feedback</h1>
@@ -36,15 +59,7 @@ const App = () => {
       <button onClick={() => setNeutralFeedback()}>Neutral</button>
       <button onClick={() => setBadFeedback()}>Bad</button>
 
-      <h1>Stats</h1>
-
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-
-      <p>Total: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positivePercentage}%</p>
+      <Statistics {...statisticsProps} />
     </div>
   );
 };
