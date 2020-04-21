b50c3c6020e7c254d41407cac02cfb2486d38bea
Mehdi Amrane <me.amrane@gmail.com>
2020-04-21 18:19:39 +0200

    Part2/phonebook: 2.9 solution added (#25)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '06-01-02-03-04' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchedName, setSearchedName] = useState('');
 
   const nameExists = persons.find((person) => person.name.toLowerCase() === newName.toLowerCase());
 
@@ -28,7 +34,16 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
-  const listOfPersons = persons.map((person) => {
+  const handleSearchedNameChange = (e) => {
+    setSearchedName(e.target.value);
+  };
+
+  const personsToDisplay =
+    searchedName.length === 0
+      ? persons
+      : persons.filter((person) => person.name.toLowerCase().includes(searchedName.toLowerCase()));
+
+  const listOfPersons = personsToDisplay.map((person) => {
     return (
       <li key={person.name}>
         {person.name} ({person.number})
@@ -39,6 +54,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Find by name: <input value={searchedName} onChange={handleSearchedNameChange} />
+      </div>
+      <h2>Add to phone book</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
